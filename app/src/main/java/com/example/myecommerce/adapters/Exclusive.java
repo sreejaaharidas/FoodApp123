b5a46f8c83ad988eb4f6a3f2fda2f9061c29db97
sreejaaharidas <72782813+sreejaaharidas@users.noreply.github.com>
2022-01-08 15:02:40 +0530

    completed

--- app/src/main/java/com/example/myecommerce/adapters/Exclusive.java
+++ app/src/main/java/com/example/myecommerce/adapters/Exclusive.java
@@ -13,7 +13,7 @@ import com.makeramen.roundedimageview.RoundedImageView;
 
 import java.util.List;
 
-public class Exclusive extends RecyclerView.Adapter<OfferAdapter.ViewHolder> {
+public class Exclusive extends RecyclerView.Adapter<Exclusive.ViewHolder> {
     List<ExcluiveModels> list;
 
     public Exclusive(List<ExcluiveModels> list) {
@@ -22,12 +22,12 @@ public class Exclusive extends RecyclerView.Adapter<OfferAdapter.ViewHolder> {
 
     @NonNull
     @Override
-    public OfferAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new OfferAdapter.ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.exclusives_item,parent,false));
+    public Exclusive.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new Exclusive.ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.exclusives_item,parent,false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull OfferAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull Exclusive.ViewHolder holder, int position) {
         holder.roundedImageView.setImageResource(list.get(position).getImg());
 
     }
